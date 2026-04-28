f5fe7e92036c2af5ca1ff79e6f9205de1766bd01
Richard Uzor <equality560@gmail.com>
2026-04-28 06:16:08 +0100

    added custom url to allowed

--- vite.config.ts
+++ vite.config.ts
@@ -12,7 +12,7 @@ export default defineConfig({
   server: {
     port: 5173,
     strictPort: true,
-    allowedHosts: ['localhost', '127.0.0.1', '.local', 'fatigued-nonleprous-hue.ngrok-free.dev', 'plastery-unhampered-erline.ngrok-free.dev'],
+    allowedHosts: ['localhost', '127.0.0.1', '.local', 'fatigued-nonleprous-hue.ngrok-free.dev', 'plastery-unhampered-erline.ngrok-free.dev', 'dev-app.spinrewardsgame.com'],
   },
   build: {
     target: 'es2020',
